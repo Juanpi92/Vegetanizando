c1b1cd43e62cf727c2852b4801e7a8d6e7f3d1a8
Juanpi92 <juan.urgelles92@gmail.com>
2023-01-15 22:36:24 -0300

    creando estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Servicos from "./pages/Servicos";
+import Error404 from "./pages/Error404";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/acerca" element={<About />}></Route>
+          <Route exact path="/servicos" element={<Servicos />}></Route>
+          <Route path="*" element={<Error404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Servicos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Servicos = () => {
+  return (
+    <>
+      <div>Servicos</div>
+    </>
+  );
+};
+
+export default Servicos;
