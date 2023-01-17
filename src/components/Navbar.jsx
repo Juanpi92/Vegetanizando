420cef2fba16e8b559aae7d5024d6e23f060fcb7
Juanpi92 <juan.urgelles92@gmail.com>
2023-01-16 21:13:20 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const $links = useRef();
+  const HandleClick = () => {
+    $links.current.classList.toggle("active");
+  };
   return (
     <>
-      <div>Navbar</div>
+      <nav className="navbar">
+        <div className="title">
+          <img src="./imagenes/background.jpg" className="logo"></img>
+          <span> Vegeteranizando</span>
+        </div>
+        <a className="toggle_button" onClick={HandleClick}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+        <div className="link" ref={$links}>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/acerca">Acerca</a>
+            </li>
+            <li>
+              <a href="/servicos">Serviços</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
