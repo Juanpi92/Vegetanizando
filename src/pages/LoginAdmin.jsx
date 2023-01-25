2aacf1c861ae9d6899afe9f76f0f2179a58efe42
Juanpi92 <juan.urgelles92@gmail.com>
2023-01-25 16:07:24 -0300

    comment

--- src/pages/LoginAdmin.jsx
+++ src/pages/LoginAdmin.jsx
@@ -8,32 +8,35 @@ const LoginAdmin = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   return (
-    <div style={{ margin: "100px 350px" }}>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          axios
-            .get(
-              "https://vegetanizando-api.onrender.com/admin?email=juan.urgelles92@gmail.com&password=Juanpi22"
-            )
-            .then((respuesta) => {
-              dispatch(setUser({ name: "Juanpi" }));
-              navigate("/admin/products");
-            })
-            .catch((error) => {
-              console.log("error");
-            });
-        }}
-      >
-        <input type="text" />
-        <br />
-        <br />
-        <input type="text" />
-        <br />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
-    </div>
+    <>
+      {/*
+      <div style={{ margin: "100px 350px" }}>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            axios
+              .get(
+                "https://vegetanizando-api.onrender.com/admin?email=juan.urgelles92@gmail.com&password=Juanpi22"
+              )
+              .then((respuesta) => {
+                dispatch(setUser({ name: "Juanpi" }));
+                navigate("/admin/products");
+              })
+              .catch((error) => {
+                console.log("error");
+              });
+          }}
+        >
+          <input type="text" />
+          <br />
+          <br />
+          <input type="text" />
+          <br />
+          <br />
+          <input type="submit" value="Login" />
+        </form>
+      </div>*/}
+    </>
   );
 };
 
