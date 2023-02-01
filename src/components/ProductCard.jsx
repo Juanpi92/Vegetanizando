0496f55030b01b1120d966182bbf346373e67f79
Juan Pablo Urgelles Pelier <113916693+Juanpi92@users.noreply.github.com>
2023-02-01 11:26:05 -0300

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -17,7 +17,7 @@ const ProductCard = ({ data }) => {
         <article className="texto_card">
           <p className="title_card">{name}</p>
           <p>Porção: {portion}</p>
-          <p className="price">Preço:R$ {price.toFixed(2)}</p>
+          <p className="price">Preço: R$ {price.toFixed(2)}</p>
         </article>
         <div className="div_button">
           <button
