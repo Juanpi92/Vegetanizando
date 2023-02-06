398b506845a74e5b56d83fcc3a9538e30b9c824f
Juan Pablo Urgelles Pelier <113916693+Juanpi92@users.noreply.github.com>
2023-02-06 10:12:11 -0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import './about.css'
 
 const About = () => {
   return (
     <>
+    <div className="image-pop">
+ <img src="" alt="chef" />
+    </div>
       <div className="container_principal">
-        <div>About</div>
+        <div><div className="container_principal servicos">
+        <div className="title_principal">
+            <p>você conhece vegetanizando ?</p>
+        </div>
+        <div className="about1">
+          <div className="about2">
+            <p>A vegetanizando é totalmente vegano e a favor das causas poiticas que apoiam o meio ambiente , e a alimentação saldaver dos seres humanos .  </p>
+          </div>
+          <div className="about">
+            <p>Somos um somos um restaurante que desponibiliza um sistema de delivery de nosso cardapio 100% natural para todos os nossos clientes , nosso restaurante vem de um plano de veganização de muitos anos mais só no ano de 2021 nos pudemos nos estabelecer como um restaurante vegano</p>
+          </div>
+          <div className="about">
+            <p>nosso primeiro restarante assim como nosso pricipal restarante foi criado em 2021 no centro de campinas-sp e com o fruto dos nossos esforços vegetanizando conseguio abrir um novo restaurante no centro do rio de janeiro ! </p>
+          </div>
+          <div className="about">
+            <p>Nossa meta é para os proximos anos ter uma loja em cada canto do brazil, para que todos os brazileiros venham desfrutar de nosso grande e saboroso restaurante 100% vegano , com pratos exclusivos que não se encontra em nenhum outro canto , ums exemplos são , a Mini carolina , aboborinha recheada e muitos outro , como um bom restaurante vegano prezamos pela saude de nossos clientes sempre com atenção e cuidado com todos os ingredientes , todos são muito bem manuziados e manipulados por proficionas muito bem qualificados e remunerandos com desempenho alticimo na area ao qual trabalham  </p>
+          </div>
+        </div>
+      </div>
+        </div>
       </div>
     </>
   );
