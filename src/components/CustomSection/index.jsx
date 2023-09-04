129dc7eb873673f17c46a24123fe89341fcf0690
Juanpi92 <juan.urgelles92@gmail.com>
2023-09-04 12:18:12 -0300

    feat(Services) Whatsapp conection function

--- src/components/CustomSection/index.jsx
+++ src/components/CustomSection/index.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import './styles.css'
-import { WhatsApp } from '@mui/icons-material';
+import React from "react";
+import "./styles.css";
+import { WhatsApp } from "@mui/icons-material";
 
 export default function CustomSection({ data }) {
-    let { id, title, text, image } = data;
+  let { id, title, text, image } = data;
 
-    return (
-        <section className='custom-section-container' id={id}>
-            <img src={image} alt="" className="custom-section-image" />
-            <div className="custom-section-content">
-                <h2 className="content-title">{title}</h2>
-                <p className="content-text">{text}</p>
-                <button className="button_principal" id='content-contact'>
-                    Entre em contato
-                    <WhatsApp />
-                </button>
-            </div>
-        </section>
-    )
+  return (
+    <section className="custom-section-container" id={id}>
+      <img src={image} alt="" className="custom-section-image" />
+      <div className="custom-section-content">
+        <h2 className="content-title">{title}</h2>
+        <p className="content-text">{text}</p>
+        <button
+          className="button_principal"
+          id="content-contact"
+          onClick={() => {
+            let menssage = `Estou interessado em contratar o serviço de ${title}`;
+            window.open(
+              `https://wa.me/5521970657460?text=${encodeURIComponent(
+                menssage
+              )}`,
+              "_blank"
+            );
+          }}
+        >
+          Entre em contato
+          <WhatsApp />
+        </button>
+      </div>
+    </section>
+  );
 }
