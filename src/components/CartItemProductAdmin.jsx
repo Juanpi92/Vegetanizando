5ab21f8d2165386dd78ba95c3c5a37ebcb1c546d
plfmoura <plfmoura96@gmail.com>
2023-09-04 23:34:33 -0300

    refactor: cart styles/added responsive

--- src/components/CartItemProductAdmin.jsx
+++ src/components/CartItemProductAdmin.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { delProduct } from "../reducer/shoopingReducer";
-import "./CartItem.css";
 
 const CartItemProductAdmin = ({
   product,
