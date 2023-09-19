e6766694f77a19f8ee4fa30719c736e2d2cee667
plfmoura <plfmoura96@gmail.com>
2023-09-19 00:13:04 -0300

    refactor: add new routes layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { useEffect } from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Servicos from "./pages/Servicos";
-import Cart from "./pages/Cart";
-import Error404 from "./pages/Error";
-import AdminCompras from "./pages/AdminCompras";
-import AdminProducts from "./pages/AdminProducts";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import {
@@ -18,7 +11,6 @@ import {
 
 import { setUser } from "./reducer/userReducer";
 import { actualizarCompras } from "./reducer/comprasReducer";
-import { LoginAdmin } from "./pages/LoginAdmin";
 import Header from "./components/Header";
 import NavigationMobile from "./components/NavigationMobile";
 import PurchaseModal from "./components/PurchaseModal";
@@ -26,11 +18,10 @@ import { AppProvider } from "./contexts/AppContext";
 import CartComprasConfirm from "./components/CartPurchase/CartComprasConfirm";
 import ScrollToTop from "./components/ScrollToTop";
 import AsideAdmin from "./components/AsideAdmin";
-import Statistics from "./pages/Statistics";
 import { setPlans } from "./reducer/plansReducer";
-import AdminPlans from "./pages/AdminPlans";
 import Alert from "./components/Alert";
 import { AnimatePresence } from "framer-motion";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   const dispatch = useDispatch();
@@ -107,56 +98,29 @@ function App() {
   }, []);
 
   return (
-    <>
-      <AnimatePresence>
-        {user ? (
-          <HashRouter>
-            <AppProvider>
-              <AsideAdmin />
-              <Alert />
-              <Routes>
-                <Route
-                  exact
-                  path="/admin/compras"
-                  element={<AdminCompras />}
-                ></Route>
-                <Route
-                  exact
-                  path="/admin/products"
-                  element={<AdminProducts />}
-                ></Route>
-                <Route exact path="/admin/plans" element={<AdminPlans />}></Route>
-                <Route
-                  exact
-                  path="/admin/statistics"
-                  element={<Statistics />}
-                ></Route>
-                <Route path="*" element={<Error404 />}></Route>
-              </Routes>
-            </AppProvider>
-          </HashRouter>
-        ) : (
-          <HashRouter>
-            <AppProvider>
-              <Header />
-              <NavigationMobile />
-              <Alert />
-              <ScrollToTop />
-              <PurchaseModal children={<CartComprasConfirm />} />
-              <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/acerca" element={<About />}></Route>
-                <Route exact path="/servicos" element={<Servicos />}></Route>
-                <Route exact path="/cart" element={<Cart />}></Route>
-                <Route exact path="/admin" element={<LoginAdmin />}></Route>
-                <Route path="*" element={<Error404 />}></Route>
-              </Routes>
-              <Footer />
-            </AppProvider>
-          </HashRouter>
-        )}
-      </AnimatePresence>
-    </>
+    <AnimatePresence>
+      <HashRouter>
+        <AppProvider>
+          <ScrollToTop />
+          <Alert />
+          {
+            user ?
+              <>
+                <AsideAdmin />
+                <AppRoutes variant={'private'} />
+              </>
+              :
+              <>
+                <Header />
+                <NavigationMobile />
+                <AppRoutes variant={'public'} />
+                <PurchaseModal children={<CartComprasConfirm />} />
+                <Footer />
+              </>
+          }
+        </AppProvider>
+      </HashRouter>
+    </AnimatePresence>
   );
 }
 
