db46be7adf16774171f02f54d05e843ed13cdfaf
plfmoura <plfmoura96@gmail.com>
2023-09-18 21:57:06 -0300

    feat: add framer motion/create error folder

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import "./Error404.css";
+import "./styles.css";
 import imagen404 from "/imagenes/404.png";
 
 const Error404 = () => {
@@ -21,7 +21,7 @@ const Error404 = () => {
         <div className="container_principal">
           <figure className="figure404">
             <img src={imagen404} alt="Error 404" className="imagen404" />
-            <figcaption>O recurso solicitado ñao foi encontrado</figcaption>
+            <figcaption>A página não foi encontrada...</figcaption>
           </figure>
         </div>
       )}
